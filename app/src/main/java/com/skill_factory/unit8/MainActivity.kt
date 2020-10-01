9a74c1faeb5de6c81e743a303cfd06d06e6bec99
vurtune <kostya.urenevar@gmail.com>
2020-10-01 14:19:38 +0400

    Initial commit

---
+++ app/src/main/java/com/skill_factory/unit8/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.skill_factory.unit8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.skill_factory.unit8.adapter.MyAdapter
+import com.skill_factory.unit8.model.Fruit
+import com.skill_factory.unit8.snap_helper.StartSnapHelper
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val recyclerView1 = findViewById<RecyclerView>(R.id.recycler_view1)
+        val snapHelper1 = LinearSnapHelper()
+        snapHelper1.attachToRecyclerView(recyclerView1)
+
+        fun data() : ArrayList<Fruit> = arrayListOf(
+            Fruit(R.drawable.ic_apple, "Apple"),
+            Fruit(R.drawable.ic_banana, "Banana"),
+            Fruit(R.drawable.ic_lemon, "Lemon"),
+            Fruit(R.drawable.ic_orange, "Orange"),
+            Fruit(R.drawable.ic_pear, "Pear"),
+            Fruit(R.drawable.ic_pear, "Pear1"),
+            Fruit(R.drawable.ic_pear, "Pear2"),
+            Fruit(R.drawable.ic_pear, "Pear3"),
+            Fruit(R.drawable.ic_strawberry, "Strawberry"),
+        )
+
+        recyclerView1.adapter = MyAdapter(data())
+
+        val recyclerView2 = findViewById<RecyclerView>(R.id.recycler_view2)
+        val snapHelper2 = PagerSnapHelper()
+        snapHelper2.attachToRecyclerView(recyclerView2)
+
+        recyclerView2.adapter = MyAdapter(data())
+    }
+}
---
+++ app/src/main/java/com/skill_factory/unit8/model/Fruit.kt
@@ -0,0 +1,5 @@
+package com.skill_factory.unit8.model
+
+import androidx.annotation.DrawableRes
+
+data class Fruit(@DrawableRes val idIcon: Int, val name: String)
